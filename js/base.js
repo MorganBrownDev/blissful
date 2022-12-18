4f55b245c29f44fdb565a133e43229217605ef15
MorganBrownDev <mjxbrown@gmail.com>
2022-12-18 15:49:41 +1100

    Update

--- js/base.js
+++ js/base.js
@@ -2,5 +2,12 @@ const menu = document.querySelector('.hamburger');
 const burger = document.querySelector('.burger');
 
 menu.addEventListener('click', function() {
-    burger.classList.toggle("active");
+    if (burger.classList.contains('active')) {
+        burger.classList.toggle("non-active");
+        document.body.classList.toggle("overflow-off");
+    }
+    else {
+        burger.classList.toggle("active");
+        document.body.classList.toggle("overflow-off");
+    }
 });
